0f53b0c4731d92be80ad5e1456e25aedaa572bd1
Lauri Junkkari <latenssi@gmail.com>
2022-03-11 12:48:50 +0200

    Add initial version of Solana build

---
+++ solana/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+REPO=eqlabs/solana
+VERSION=1.9.11
+
+docker buildx build \
+  --platform linux/amd64,linux/arm64,linux/arm/v7 \
+  --build-arg SOLANA_VERSION=${VERSION} \
+  -t ${REPO}:${VERSION} \
+  --push \
+  .
